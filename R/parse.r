85a3f813ce802973a5f008b77c42c9580e1cfcc1
hadley <h.wickham@gmail.com>
2013-09-25 13:19:38 -0500

    Collapsed parsed data

--- R/parse.r
+++ R/parse.r
@@ -10,26 +10,38 @@ parse_prof <- function(path) {
   
   # Separate file labels and profiling data
   is_label <- raw$V1 != ""
-  
+
   labels <- raw[is_label, 1:2]
   names(labels) <- c("label", "path")
   labels$label <- as.numeric(str_replace_all(labels$label, "[^0-9]+", ""))
   labels$path <- str_trim(labels$path)
   rownames(labels) <- NULL
   
+  # Parse profiling data -----------------
   prof <- raw[!is_label, ]
   prof$V1 <- NULL
   prof$V2 <- as.numeric(prof$V2)
   names(prof) <- c("small_v", "big_v", "nodes", "dups", "source")
   
   # Add time info, and compute total memory (rounded to meg's)
-  prof$time <- 1:nrow(prof) * interval
+  prof$time <- interval
   prof$mem <- (prof$small_v + prof$big_v + prof$nodes) / 1024 ^ 2
   
   # Compute memory allocation and release
   mem_alloc <- c(diff(prof$mem), NA)
   prof$alloc <- pmax(0, mem_alloc)
   prof$release <- abs(pmin(0, mem_alloc))
+  prof$mem <- NULL
+  
+  # Strip <GC> and collapse to one row per ref
+  prof$source <- str_replace_all(prof$source, '"<GC>" ?', "")
+  grp <- cumsum(c(FALSE, prof$source[-nrow(prof)] != prof$source[-1]))
+  profsum <- aggregate(
+    prof[c("time", "alloc", "release", "dups")], 
+    list(g = grp), 
+    sum)
+  profsum$g <- NULL
+  profsum$source <- prof$source[!duplicated(grp)]
   
   list(mem = prof, paths = labels$path)
 }
