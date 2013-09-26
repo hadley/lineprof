823191e3e36d05fb760349b9766bd63d53622966
hadley <h.wickham@gmail.com>
2013-09-26 09:03:36 -0500

    Remove duplicated refs

--- R/parse-ref.r
+++ R/parse-ref.r
@@ -4,6 +4,19 @@ parse_ref <- function(ref, paths) {
   f <- parsed[[1]]
   ref <- parsed[[2]]
   
+  # Remove last duplicated refs
+  # e.g. :197781:316585:19036808:0:"<GC>" 1#3 "scan" 1#3 "read_delim"
+  # 
+  # From Duncan Murdoch:
+  # What that means is that scan was active, but it was evaluating something 
+  # which appeared to be from this line:
+  # 
+  # first <- scan(file, what = character(1), nlines = 1, sep = sep, 
+  #   quiet = TRUE)
+  #
+  # It could have been evaluating one of the arguments...
+  ref[duplicated(ref, fromLast = TRUE)] <- ""
+  
   # Split refs into path and line
   ref <- str_split_fixed(ref, "#", n = 2)
   path <- paths[as.numeric(ref[, 1])]  
