c95a560c0a612c8593ebb143724e6cc8c04ffba9
hadley <h.wickham@gmail.com>
2013-09-26 14:10:05 -0500

    Documentation brain dump

--- R/lineprof.r
+++ R/lineprof.r
@@ -1,8 +1,47 @@
 #' Line profiling.
 #' 
+#' \code{lineprof} uses R built-in time and memory profiler \code{\link{Rprof}}
+#' to collect line profiling data and displays it in ways that help you
+#' figure out where the bottlenecks in your code R.
+#' 
+#' R's profiler is a sampling profiler, which means that it stops execution
+#' every \code{interval} seconds and records the current call stack. This means
+#' that it is somewhat random: if you run the profiler repeatedly on the same
+#' code you'll see slightly different outputs depending on exactly where the 
+#' profiler stopped each time.
+#' 
+#' @section Profiling output:
+#' 
+#' For each sequence of calls, \code{lineprof} calculates:
+#' 
+#' \itemize{
+#'   \item \code{time}: the time in seconds
+#'   \item \code{alloc}: the memory allocated, in megabytes
+#'   \item \code{released}: the memory released, in megabytes. Unless
+#'     \code{torture = TRUE} this release is somewhat random:
+#'     memory will be only released if a garbage collection is triggered by
+#'     an allocation.
+#'   \item \code{dups}: the number of calls to the internal \code{duplicate}
+#'     function which is called by C code to duplicate R vectors.
+#' }
+#' 
+#' Note that memory allocation is only approximate due to the nature of the 
+#' sampling profiler: if a large block of memory is allocated and then released
+#' in between ticks of the timer, no change in memory will be recorded. Using
+#' \code{torture = TRUE} helps prevent this, mostly by making R super slow.
+#' 
+#' @param code code to profile.
+#' @param interval interval, in seconds, between profile snapshots. R's timer
+#'   has a resolution of at best 1 ms, so there's no reason to make this value
+#'   smaller, but if you're profiling a long running function you might want to
+#'   set it to something larger.
+#' @param torture if \code{TRUE}, turns on \code{\link{gctorture}} which forces
+#'   \code{\link{gc}} to run after (almost) every allocation. This is useful
+#'   if you want to see exactly when memory is released. It also makes R run
+#'   extremely slowly (10-100x slower than usual) so it can also be useful to
+#'   simulate a smaller \code{interval}.
 #' @export
 #' @examples
-#' 
 #' source(find_demo("read-delim.r"))
 #' source(find_demo("read-table.r"))
 #' wine <- find_demo("wine.csv")
