2eeaa777fe541a8eb4464b3ef97d7fe83c5e499a
hadley <h.wickham@gmail.com>
2013-09-30 07:41:21 -0500

    Improvements to CSS

--- inst/www/format-table.js
+++ inst/www/format-table.js
@@ -14,7 +14,7 @@ escape_string = function(text) {
 var cols = [
   {id: "src", name: "Source code", field: "src", width: 450, cssClass: "code"},
   {id: "time", name: "t", field: "time", width: 60},
-  {id: "release", name: "r", field: "release", width: 30, cssClass: "right"},
+  {id: "release", name: "r", field: "release", width: 30, cssClass: "negative"},
   {id: "alloc", name: "a", field: "alloc", width: 30},
   {id: "dups", name: "d", field: "dups", width: 30}
 ];
@@ -58,10 +58,10 @@ Shiny.addCustomMessageHandler("formatTable",
         d["src"] = escape_html(data.src[i]);
       }
       
-      d["time"] = percent_bar(data.time[i], max_time, "positive-bar");
-      d["release"] = percent_bar(data.release[i], max_release, "negative-bar");
-      d["alloc"] = percent_bar(data.alloc[i], max_alloc, "positive-bar");
-      d["dups"] = percent_bar(data.dups[i], max_dups, "positive-bar");
+      d["time"] = percent_bar(data.time[i], max_time);
+      d["release"] = percent_bar(data.release[i], max_release);
+      d["alloc"] = percent_bar(data.alloc[i], max_alloc);
+      d["dups"] = percent_bar(data.dups[i], max_dups);
     }
     
     grid = new Slick.Grid("#profile", rows, cols, slickGridOpts);
@@ -69,7 +69,7 @@ Shiny.addCustomMessageHandler("formatTable",
 );
 
 var percent_bar = function(val, max, spanClass) {
-  return "<span class='" + spanClass + "' " + 
+  return "<span class='bar' " + 
     "style = 'width:" + (val / max) * 100 + "%' " + 
     "title = '" + val + "'></span>";
 };
