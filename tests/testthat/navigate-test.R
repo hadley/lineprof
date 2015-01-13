d3bc24a017f62306980437e11a7ee95f17069970
Saurfang <forest.fang@outlook.com>
2015-01-13 06:37:44 -0500

    document test script

--- tests/testthat/navigate-test.R
+++ tests/testthat/navigate-test.R
@@ -1,3 +1,6 @@
+#' Use eval(parse(text="...")) here because we want to test navigate using function
+#' call stacks instead of line numbers. The following will cause Rprof to use
+#' <text>#linenumber as refs which ultimately fall back on function call stacks.
 path <- line_profile(eval(parse(text = "
     quick <- function() { identity(identity(pause(.1))) }
     slow <- function() { identity(identity(pause(.2))) }
